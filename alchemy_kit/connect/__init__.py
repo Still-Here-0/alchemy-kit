e77bbd2bdb9cd65dc114f87b0ab8c4d88bd8c060
Bruno Pinho <bruno.pinho@linde.com>
2026-07-13 13:31:17 -0300

    Adding info_builder into connect module

--- alchemy_kit/connect/__init__.py
+++ alchemy_kit/connect/__init__.py
@@ -1,8 +1,10 @@
+from . import info_builder
 from ._engine_handler import EngineHandler
 from ._engine_manager import EngineManager
 from ._info import ConnectionInfo
 
 __all__ = [
+    "info_builder",
     "EngineHandler",
     "EngineManager",
     "ConnectionInfo",
